897f05ae863506524b1a416638d32756a47ec077
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-25 18:19:28 +0300

    Created the header and styled it

---
+++ dmResumeApp/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function header() {
+  return (
+    <header className='header'>
+        <div className='logo'>
+            <h4>DM</h4>
+            <h1>RESUME</h1>
+        </div>
+    </header>
+  )
+}
+
+export default header
--- dmResumeApp/src/main.jsx
+++ dmResumeApp/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Footer from './components/footer.jsx'
+import Header from './components/header.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     <App />
     <Footer />
   </React.StrictMode>,
