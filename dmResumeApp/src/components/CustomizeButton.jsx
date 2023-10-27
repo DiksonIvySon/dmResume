f12e9f3a52d409d8f08024b1889aae83ffd03e2e
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-27 08:17:07 +0300

    Created a function to toggle the form visibility in the customize button

--- dmResumeApp/src/components/CustomizeButton.jsx
+++ dmResumeApp/src/components/CustomizeButton.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Buttons.css'
 import downArrow from './icons/downArrow.png'
 import customize from './icons/customize.png'
 
 
 function CustomizeButton() {
+  const [visibility, setVisibility] = useState("hide-element")
+
+  //toggle visibility of the form
+  const handleVisibility = () => {
+    if (visibility === "hide-element") {
+      const newVisibility = "show-element"
+      setVisibility(newVisibility)
+    }
+    else {
+      const newVisibility = "hide-element"
+      setVisibility(newVisibility)
+    }
+  }
+
   return (
     <div className='CustomizeButton'>
         <div className='mainButton'>
@@ -12,9 +26,13 @@ function CustomizeButton() {
               <img src={customize} alt="" />
               <p>Customize Resume</p>
           </div>
-          <div>
+          <div className='downArrow' onClick={handleVisibility}>
               <img src={downArrow} alt="" />
           </div>
+        </div>
+        <div className={visibility} >
+          <hr />
+          <h1>man</h1>
         </div>
     </div>
   )
