b89cda3f458d92734d1174ac857737913f813d27
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-27 14:38:33 +0300

    Created the contents of the Customize button and styled it

--- dmResumeApp/src/components/CustomizeButton.jsx
+++ dmResumeApp/src/components/CustomizeButton.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './Buttons.css'
+import './CustomizeButton.css'
 import downArrow from './icons/downArrow.png'
 import customize from './icons/customize.png'
 
@@ -32,7 +33,77 @@ function CustomizeButton() {
         </div>
         <div className={visibility} >
           <hr />
-          <h1>man</h1>
+          <div className='fonts-container'>
+            <h1>Fonts</h1>
+            <div>
+                <div className='font serif'>
+                  <h1>Aa</h1>
+                  <p>Serif</p>
+                </div>
+                <div className='font sans-serif'>
+                  <h1>Aa</h1>
+                  <p>Sans</p>
+                </div>
+                <div className='font time-new-roman'>
+                  <h1>Aa</h1>
+                  <p>Time New Roman</p>
+                </div>
+                <div className='font arial'>
+                  <h1>Aa</h1>
+                  <p>Arial</p>
+                </div>
+                <div className='font cochin'>
+                  <h1>Aa</h1>
+                  <p>Calibri</p>
+                </div>
+            </div>
+          </div>
+          <div className='colors-container'>
+              <h1>Colors</h1>
+              <div className='color-items'>
+                  <div className='color-item'>
+                        <div className='color-picker'>
+                          <input type="color" value="pink" />
+                        </div>
+                        <h3>Accent</h3>
+                  </div>
+                  <div className='color-item'>
+                        <div className='color-picker'>
+                          <input type="color" value="gray" />
+                        </div>
+                        <h3>Text</h3>
+                  </div>
+                  <div className='color-item'>
+                        <div className='color-picker'>
+                          <input type="color" value="pink" />
+                        </div>
+                        <h3>Bold Text</h3>
+                  </div>
+                  <div className='color-item'>
+                        <div className='color-picker'>
+                          <input type="color" value="pink" />
+                        </div>
+                        <h3>Headings</h3>
+                  </div>
+              </div>
+          </div>
+          <div className='layouts-container'>
+            <h1>Layouts</h1>
+            <div className='layouts'>
+              <div className='horizontal-layout'>
+                  <div className='accent'><p>Accent</p></div>
+                  <div className='content'></div>
+              </div>
+              <div className='vertical-layout'>
+                  <div className='accent'><p>Accent</p></div>
+                  <div className='content'></div>
+              </div>
+              <div className='switch-layout'>
+                  <div className='accent'><p>Accent</p></div>
+                  <div className='content'></div>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   )
