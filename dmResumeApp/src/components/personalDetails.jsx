2569fab680ca6cd45d1ce64247a34d1f661367fa
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2023-10-28 12:56:22 +0300

    Created the personal details form

--- dmResumeApp/src/components/personalDetails.jsx
+++ dmResumeApp/src/components/personalDetails.jsx
@@ -1,10 +1,16 @@
 import React, {useState} from 'react'
 import './Buttons.css'
+import './PersonalDetails.css'
 import downArrow from './icons/downArrow.png'
 import personal_Details from './icons/personalDetails.png'
 
 function personalDetails() {
-  const [visibility, setVisibility] = useState("hide-element")
+  const [visibility, setVisibility] = useState("hide-element");
+  const [name, setName] = useState("Dikson");
+  const [position, setPosition] = useState("Front-End Dev");
+  const [email, setEmail] = useState("ivyCodes@gmail");
+  const [number, setNumber] = useState("0750638263");
+  const [location, setLocation] = useState("Joburg")
 
   //toggle visibility of the form
   const handleVisibility = () => {
@@ -31,7 +37,47 @@ function personalDetails() {
       </div>
       <div className={visibility} >
           <hr />
-          <h1>man</h1>
+          <form className='personalDetails-form'>
+            <label>Full Name: <span>Required</span> <br />
+              <input
+                type="text" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <label>Position: <span>Required</span> <br />
+              <input
+                type="text" 
+                value={position}
+                onChange={(e) => setPosition(e.target.value)}
+              />
+            </label>
+            <label>E-mail: <span>Required</span> <br />
+              <input
+                type="email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label>Contact Number: <span>Required</span> <br />
+              <input
+                type="number" 
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+              />
+            </label>
+            <label>Location: <span>Recommended</span> <br />
+              <input
+                type="text" 
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </label>
+            <div className='form-buttons'>
+                <button type='reset'>Reset</button>
+                <button type='button'>Done</button>
+            </div>
+          </form>
         </div>
     </div>
   )
