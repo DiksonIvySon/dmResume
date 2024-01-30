9e13b84c7fd3833b3883362e2a71a471b6a7cc66
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2024-01-30 10:44:24 +0300

    Created the landing page.

--- dmResumeApp/src/components/Header.jsx
+++ dmResumeApp/src/components/Header.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Header.css'
 
+
 function header() {
   return (
-    <header className='header'>
+    <header className='header section-margin'>
         <div className='logo'>
             <h4>DM</h4>
             <h1>RESUME</h1>
         </div>
+        <div className='headerLinks'>
+          <a href="">
+            <p>Log In</p>
+          </a>
+          <a href="" >
+            <button className='button2'>Build A Resume</button>
+          </a>
+        </div>
+
     </header>
   )
 }
--- dmResumeApp/src/pages/HomePage.jsx
+++ dmResumeApp/src/pages/HomePage.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import Footer from '../components/footer.jsx'
-import Header from '../components/header.jsx'
+import './HomePage.css';
+import Footer from '../components/footer.jsx';
+import Header from '../components/header.jsx';
+import background1 from '../images/background1.jpg';
+import cv_image1 from '../images/cv-image1.png';
 
 function HomePage() {
   return (
     <div className='HomePage'>
         <Header />
+        <section className='landingPage'>
+            <div className='background1'>
+                <img src={background1} alt="" />
+            </div>
+            <div className='landingPage-content section-margin'>
+                <div>
+                    <h2>Fast. Easy. Effective.</h2>
+                    <h1>dmResume. The Best CV Maker Online.</h1>
+                    <p>
+                        If a sheet of paper represents your entire work life, personality, and skills, 
+                        it better be a pretty amazing piece of paper — Let dmResume do the heavy lifting.
+                    </p>
+                    <button className='button2'>Create your CV</button>
+                </div>
+                <div className='cv-image1'>
+                    <img src={cv_image1} alt="Resume image" />
+                </div>
+            </div>
+        </section>
+
         
-        <Footer />
+        {/*}<Footer />}{*/}
     </div>
   )
 }
